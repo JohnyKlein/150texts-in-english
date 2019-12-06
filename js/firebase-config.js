e6317c514dd9d25a135e0083100766d6bbd06d24
Johny Klein <johnyklein@hotmail.com>
2019-12-05 21:58:18 -0300

    Iniciado config firebase

---
+++ js/firebase-config.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyD0yyU37y5oLuroWrXZsYcNyUypwCJwVd0",
+  authDomain: "delivery-67901.firebaseapp.com",
+  databaseURL: "https://delivery-67901.firebaseio.com",
+  projectId: "delivery-67901",
+  storageBucket: "delivery-67901.appspot.com",
+  messagingSenderId: "1011107737410",
+  appId: "1:1011107737410:web:740bfc1a3427bee48ee2db"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var db = firebase.firestore();
--- js/index.js
+++ js/index.js
@@ -1,60 +1,59 @@
-var videoUrl = 'dJaJUAGy47Q';
-var videos = [
-  {
-    name: '#1-First Snow Fall',
-    id: videoUrl,
-    startSeconds: 350,
-    endSeconds: 420
-  },
-  {
-    name: '#2-Jessica\'s First Day of School',
-    id: videoUrl,
-    startSeconds: 422,
-    endSeconds: 531
-  },
-  {
-    name: '#3-My Flower Garden',
-    id: videoUrl,
-    startSeconds: 535,
-    endSeconds: 593
-  },
-  {
-    name: '#4-Going Camping',
-    id: videoUrl,
-    startSeconds: 597,
-    endSeconds: 665
-  },
-  {
-    name: '#5-My House',
-    id: videoUrl,
-    startSeconds: 665,
-    endSeconds: 740
-  },
-  {
-    name: '#6-My First Pet',
-    id: videoUrl,
-    startSeconds: 741,
-    endSeconds: 805
-  },
-  {
-    name: '#7-Jennifer the Firefighter',
-    id: videoUrl,
-    startSeconds: 807,
-    endSeconds: 105
-  },
-  {
-    name: '#8-Mark\'s Big Game',
-    id: videoUrl,
-    startSeconds: 1007,
-    endSeconds: 1130
-  },
-  {
-    name: '#9-The Easter Egg Hunt',
-    id: videoUrl,
-    startSeconds: 1510,
-    endSeconds: 1900
-  }
-];
+// var videos = [
+//   {
+//     name: '#1-First Snow Fall',
+//     id: videoUrl,
+//     startSeconds: 350,
+//     endSeconds: 420
+//   },
+//   {
+//     name: '#2-Jessica\'s First Day of School',
+//     id: videoUrl,
+//     startSeconds: 422,
+//     endSeconds: 531
+//   },
+//   {
+//     name: '#3-My Flower Garden',
+//     id: videoUrl,
+//     startSeconds: 535,
+//     endSeconds: 593
+//   },
+//   {
+//     name: '#4-Going Camping',
+//     id: videoUrl,
+//     startSeconds: 597,
+//     endSeconds: 665
+//   },
+//   {
+//     name: '#5-My House',
+//     id: videoUrl,
+//     startSeconds: 665,
+//     endSeconds: 740
+//   },
+//   {
+//     name: '#6-My First Pet',
+//     id: videoUrl,
+//     startSeconds: 741,
+//     endSeconds: 805
+//   },
+//   {
+//     name: '#7-Jennifer the Firefighter',
+//     id: videoUrl,
+//     startSeconds: 807,
+//     endSeconds: 105
+//   },
+//   {
+//     name: '#8-Mark\'s Big Game',
+//     id: videoUrl,
+//     startSeconds: 1007,
+//     endSeconds: 1130
+//   },
+//   {
+//     name: '#9-The Easter Egg Hunt',
+//     id: videoUrl,
+//     startSeconds: 1510,
+//     endSeconds: 1900
+//   }
+// ];
 
 //Start Youtube API
 var tag = document.createElement('script');
@@ -67,38 +66,45 @@ var startClick = false;
 
 function onYouTubeIframeAPIReady(){
   var count = 0;
-  jQuery(".youtube-video").each(function(i, obj)  {
-    $(`#caption${count}`).html(videos[count].name);
-    players[obj.id] = new YT.Player(obj.id, {      
-        playerVars: {
-          controls: 2,
-          rel:0,
-          autohide:1,
-          showinfo: 0 ,
-          modestbranding: 1,
-          wmode: "transparent",
-          html5: 1
-        },    
-        events: {
-          'onReady': onPlayerReady,
-          'onStateChange': onPlayerStateChange
-        }
+
+  db.collection("videos").orderBy("order").get().then(function(querySnapshot) {
+
+    jQuery(".youtube-video").each(function(i, obj)  {
+      querySnapshot.forEach(function(doc) {
+        let video = doc.data();
+        $(`#caption${count}`).html(video.name);
+        players[obj.id] = new YT.Player(obj.id, {     
+          playerVars: {
+            controls: 2,
+            rel: 0,
+            autohide: 1,
+            showinfo: 0 ,
+            modestbranding: 1,
+            wmode: "transparent",
+            html5: 1
+          },    
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        });
+        count++;
       });
-      count++;
     });
-  }
+
+  });
+}
 
 //Functions for Event's API
 function onPlayerReady(event) {
   var id = getIdFrame(event);
-  event.target.cueVideoById(getVideo(id));
+  var video = getVideo(id);
+  event.target.cueVideoById(video);
 }
 
 function onPlayerStateChange(event) {
   startedVerify(event);
   loopedVerify(event);
-  //Removido para troca de vídeo a todo momento
-  // carouselEvents(event);
 };
 
 function startedVerify(event){
@@ -112,16 +118,21 @@ function startedVerify(event){
 function loopedVerify(event){
   if (event.data === YT.PlayerState.ENDED && shouldedRepeat()) {
     var id = getIdFrame(event);
-    event.target.loadVideoById(getVideo(id));
+    var video = getVideo(id);
+    event.target.loadVideoById(video);
   }
 }
 
-function getVideo(index) {
-  return {
-    videoId: videos[index].id,
-    startSeconds: videos[index].startSeconds,
-    endSeconds: videos[index].endSeconds
-  };
+async function getVideo(index) {
+  const videos = [];
+  await db.collection("videos").orderBy("order").get()
+  .then(function(querySnapshot) {
+    querySnapshot.docs.map(function(doc) {
+      videos.push(doc.data());
+    });
+   });
+  
+   return videos[index]
 }
 
 function getIdFrame(event) {
@@ -131,53 +142,6 @@ function getIdFrame(event) {
 function shouldedRepeat(){
   return $('#ipt-repeat:checked').val() === "on";
 }
-
-//Functions for Carousel
-function carouselEvents(event) {
-  var target_control =  jQuery(event.target.getIframe()).parent().parent().parent().find(".controls");
-  var target_caption = jQuery(event.target.getIframe()).parent().find(".carousel-caption");
-  
-  // switch(event.data){
-  //   case -1:
-  //     jQuery(target_control).fadeIn(500);
-  //     jQuery(target_control).children().unbind('click');
-  //     break
-  //   case 0:
-  //     jQuery(target_control).fadeIn(500);
-  //     jQuery(target_control).children().unbind('click');
-  //     break;
-  //   case 1:
-  //     jQuery(target_control).children().click(function () {return false;});
-  //     jQuery(target_caption).fadeOut(500);
-  //     jQuery(target_control).fadeOut(500);
-  //     break;
-  //     case 2:
-  //       jQuery(target_control).fadeIn(500);
-  //       jQuery(target_control).children().unbind('click'); 
-  //       break;
-  //     case 3:
-  //       jQuery(target_control).children().click(function () {return false;});
-  //       jQuery(target_caption).fadeOut(500);
-  //       jQuery(target_control).fadeOut(500);
-  //       break;
-  //     case 5:
-  //       jQuery(target_control).children().click(function () {return false;});
-  //       jQuery(target_caption).fadeOut(500);
-  //       jQuery(target_control).fadeOut(500);
-  //       break;
-  //     default:
-  //       break;
-  //   }
-}
-
-// jQuery(window).bind('load', function(){
-//   jQuery(".carousel-caption").fadeIn(500);
-//   jQuery(".controls").fadeIn(500);
-// });
-
-// jQuery('.carousel').bind('slid.bs.carousel', function (event) {
-//   jQuery(".controls").fadeIn(500);
-// });
 
 //General DOM Event's
 $(document).ready(function() {
